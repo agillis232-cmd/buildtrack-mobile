62469a556a884b300b3435ed1a03d82e3a847f96
Andrew Gillis <agillis232@gmail.com>
2026-05-17 21:25:03 -0700

    update bluebeam to cloud app

--- lib/openDocument.ts
+++ lib/openDocument.ts
@@ -19,23 +19,33 @@ export function openDocument(fileUrl: string, mimeType: string) {
         text: "Bluebeam Revu",
         onPress: () => {
           Linking.openURL(bluebeamUrl).catch(() => {
-            Alert.alert(
-              "Bluebeam Not Installed",
-              "Would you like to download Bluebeam Revu?",
-              [
-                {
-                  text: "Download",
-                  onPress: () => {
-                    if (Platform.OS === "ios") {
-                      Linking.openURL("https://apps.apple.com/us/app/bluebeam-revu/id353071036")
-                    } else {
-                      Linking.openURL("https://play.google.com/store/apps/details?id=com.bluebeam.revu")
+           Alert.alert(
+    "Open PDF",
+    "How would you like to open this file?",
+    [
+      {
+        text: "Bluebeam Cloud",
+        onPress: () => {
+          Linking.openURL(`bluebeamcloud://open?url=${encodedUrl}`).catch(() => {
+            Linking.openURL(`bluebeam://open?url=${encodedUrl}`).catch(() => {
+              Alert.alert(
+                "Bluebeam Not Installed",
+                "Would you like to download Bluebeam Cloud?",
+                [
+                  {
+                    text: "Download",
+                    onPress: () => {
+                      if (Platform.OS === "ios") {
+                        Linking.openURL("https://apps.apple.com/us/app/bluebeam-cloud/id1479484276")
+                      } else {
+                        Linking.openURL("https://play.google.com/store/apps/details?id=com.bluebeam.revu")
+                      }
                     }
-                  }
-                },
-                { text: "Cancel", style: "cancel" }
-              ]
-            )
+                  },
+                  { text: "Cancel", style: "cancel" }
+                ]
+              )
+            })
           })
         }
       },
@@ -46,4 +56,3 @@ export function openDocument(fileUrl: string, mimeType: string) {
       { text: "Cancel", style: "cancel" }
     ]
   )
-}
